39a54bc6c3183af2e0d5b780e01e2093c7e6e0ca
AKshayAmakein <akshay.p@amakein.com>
2021-05-21 15:26:34 +0530

    update: 5 colors added ,appBar icon and text theme added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+const primaryColor = Color(0xff089a9f);
+const whiteColor = Colors.white;
+const btnColor = Color(0xff23C0C6);
+const darkGrey = Color(0xff8598A0);
+const lightGrey = Color(0xffB0BEC5);
