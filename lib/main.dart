c665e4ace0981bf9381bed714b4b592ce9ccfcac
Piyush <piyush.17bme1022@abes.ac.in>
2021-05-25 12:49:02 +0530

    WelcomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geraki/constants/colors.dart';
 import 'package:geraki/screens/homepage.dart';
+import 'package:geraki/screens/welcomeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
           appBarTheme: AppBarTheme(
             iconTheme: IconThemeData(color: whiteColor),
           )),
-      home: MyHomePage(),
+      home: WelcomeScreen(),
     );
   }
 }
