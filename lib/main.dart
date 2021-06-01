8d58eb3448d884d28669eb863e242d0a9194b1a1
Piyush <piyush.17bme1022@abes.ac.in>
2021-06-01 12:55:32 +0530

    Profile Setup Loading and Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:geraki/constants/colors.dart';
 import 'package:geraki/controller/auth_controller.dart';
 import 'package:geraki/screens/home_screen_main.dart';
+import 'package:geraki/screens/profile_setup.dart';
 import 'package:geraki/screens/welcome_screen.dart';
 import 'package:get/get.dart';
 
@@ -83,7 +84,8 @@ class _MyAppState extends State<MyApp> {
           iconTheme: IconThemeData(color: whiteColor),
         ),
       ),
-      home: _user == null ? WelcomeScreen() : HomeScreenMain(),
+      home: ProfileSetup()
+      //_user == null ? WelcomeScreen() : HomeScreenMain(),
     );
   }
 }
