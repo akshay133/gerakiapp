92f0e47bc17885f0fac0a7ee2a3ec9608e669cde
Piyush <piyush.17bme1022@abes.ac.in>
2021-06-02 13:44:55 +0530

    Camera bug fixed

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -72,156 +72,156 @@ class _CameraScreenState extends State<CameraScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Column(
+      child: Stack(
         children: [
-          Stack(
-            children: [
-              Container(
-                  height: screenHeight * 0.923,
-                  child: CameraPreview(controller)),
-              Positioned(
-                  top: 50,
-                  left: 10,
-                  child: Row(
-                    children: [
-                      CircleImg(
-                          "https://blueandgreentomorrow.com/wp-content/uploads/2017/12/sustainable-roads-motorways.jpg"),
-                      SizedBox(
-                        width: screenWidth * 0.015,
-                      ),
-                      // Text(
-                      //   recordType == 0 ? "" : "Rec.$_start",
-                      //   style: TextStyle(color: Color(0xffF61304)),
-                      // )
-                    ],
-                  )),
-              Positioned(
-                right: 50,
-                bottom: 15,
-                left: 50,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: primaryColor),
-                        color: whiteColor.withOpacity(0.3),
-                      ),
-                      child: IconButton(
-                        color: Colors.white,
-                        iconSize: 30,
-                        icon: flashicon,
-                        onPressed: () {
-                          HapticFeedback.lightImpact();
-                          if (flashType == 0) {
-                            controller.setFlashMode(FlashMode.torch);
-                            flashType = 1;
-                            setState(() {
-                              flashicon = Icon(Icons.flash_off);
-                            });
-                          } else {
-                            controller.setFlashMode(FlashMode.off);
-                            flashType = 0;
-                            setState(() {
-                              flashicon = Icon(Icons.flash_on);
-                            });
-                          }
-                        },
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(color: primaryColor),
-                        color: whiteColor.withOpacity(0.3),
-                      ),
-                      child: IconButton(
-                        color: Colors.white,
-                        iconSize: 30,
-                        icon: Icon(Icons.flip_camera_android),
-                        onPressed: () {
-                          HapticFeedback.lightImpact();
-                          cameraType == 0 ? cameraType = 1 : cameraType = 0;
-                          setCameraType();
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 50,
-                left: 50,
-                child: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      buildCemaraElips(
-                        () async {
-                          HapticFeedback.lightImpact();
-                          final path = join(
-                            // Store the picture in the temp directory.
-                            // Find the temp directory using the `path_provider` plugin.
-                            (await getTemporaryDirectory()).path,
-                            '${DateTime.now()}.png',
-                          );
-                          file = await controller.takePicture();
-                          print(file);
-                          file.saveTo(path).then((saveImgPath) {
-                            print(path);
-                            Get.to(PreviewScreen(
-                              imagePath: path,
-                            ));
-                          });
-                        },
-                      ),
-                      SizedBox(width: screenWidth * 0.03),
-                      InkWell(
-                        onTap: () async {
-                          HapticFeedback.lightImpact();
-                          if (recordType == 0) {
-                            await controller.startVideoRecording();
-                            recordType = 1;
-                            setState(() {
-                              videoShape = BoxShape.circle;
-                            });
-                          } else {
-                            stopvideo();
-
-                            recordType = 0;
-                            setState(() {
-                              videoShape = BoxShape.circle;
-                            });
-                          }
-                        },
-                        child: Container(
-                          width: screenWidth * 0.15,
-                          height: screenHeight * 0.10,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: whiteColor,
-                              border: Border.all(color: primaryColor)),
-                          child: Center(
-                            child: Container(
-                              width: screenWidth * 0.05,
-                              height: screenHeight * 0.05,
-                              decoration: BoxDecoration(
-                                  shape: videoShape,
-                                  color: recordType == 0
-                                      ? Color(0xffF61304)
-                                      : Colors.black45),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
+          Container(
+            height : screenHeight,
+            child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: CameraPreview(controller),
+            ),
+          ),
+          Positioned(
+              top: 50,
+              left: 10,
+              child: Row(
+                children: [
+                  CircleImg(
+                      "https://blueandgreentomorrow.com/wp-content/uploads/2017/12/sustainable-roads-motorways.jpg"),
+                  SizedBox(
+                    width: screenWidth * 0.015,
+                  ),
+                  // Text(
+                  //   recordType == 0 ? "" : "Rec.$_start",
+                  //   style: TextStyle(color: Color(0xffF61304)),
+                  // )
+                ],
+              )),
+          Positioned(
+            right: 50,
+            bottom: 15,
+            left: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: primaryColor),
+                    color: whiteColor.withOpacity(0.3),
+                  ),
+                  child: IconButton(
+                    color: Colors.white,
+                    iconSize: 30,
+                    icon: flashicon,
+                    onPressed: () {
+                      HapticFeedback.lightImpact();
+                      if (flashType == 0) {
+                        controller.setFlashMode(FlashMode.torch);
+                        flashType = 1;
+                        setState(() {
+                          flashicon = Icon(Icons.flash_off);
+                        });
+                      } else {
+                        controller.setFlashMode(FlashMode.off);
+                        flashType = 0;
+                        setState(() {
+                          flashicon = Icon(Icons.flash_on);
+                        });
+                      }
+                    },
                   ),
                 ),
-              )
-            ],
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: primaryColor),
+                    color: whiteColor.withOpacity(0.3),
+                  ),
+                  child: IconButton(
+                    color: Colors.white,
+                    iconSize: 30,
+                    icon: Icon(Icons.flip_camera_android),
+                    onPressed: () {
+                      HapticFeedback.lightImpact();
+                      cameraType == 0 ? cameraType = 1 : cameraType = 0;
+                      setCameraType();
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
+          Positioned(
+            bottom: 0,
+            right: 50,
+            left: 50,
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  buildCemaraElips(
+                    () async {
+                      HapticFeedback.lightImpact();
+                      final path = join(
+                        // Store the picture in the temp directory.
+                        // Find the temp directory using the `path_provider` plugin.
+                        (await getTemporaryDirectory()).path,
+                        '${DateTime.now()}.png',
+                      );
+                      file = await controller.takePicture();
+                      print(file);
+                      file.saveTo(path).then((saveImgPath) {
+                        print(path);
+                        Get.to(PreviewScreen(
+                          imagePath: path,
+                        ));
+                      });
+                    },
+                  ),
+                  SizedBox(width: screenWidth * 0.03),
+                  InkWell(
+                    onTap: () async {
+                      HapticFeedback.lightImpact();
+                      if (recordType == 0) {
+                        await controller.startVideoRecording();
+                        recordType = 1;
+                        setState(() {
+                          videoShape = BoxShape.circle;
+                        });
+                      } else {
+                        stopvideo();
+
+                        recordType = 0;
+                        setState(() {
+                          videoShape = BoxShape.circle;
+                        });
+                      }
+                    },
+                    child: Container(
+                      width: screenWidth * 0.15,
+                      height: screenHeight * 0.10,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: whiteColor,
+                          border: Border.all(color: primaryColor)),
+                      child: Center(
+                        child: Container(
+                          width: screenWidth * 0.05,
+                          height: screenHeight * 0.05,
+                          decoration: BoxDecoration(
+                              shape: videoShape,
+                              color: recordType == 0
+                                  ? Color(0xffF61304)
+                                  : Colors.black45),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     ));
